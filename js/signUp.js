f7b4550352db85121635833b547f2b4dc0b763c9
Sven <124936934+sven0227@users.noreply.github.com>
2024-01-26 15:07:38 +0900

    test cms api

--- js/signUp.js
+++ js/signUp.js
@@ -1,1 +1,13 @@
-alert("this is custom js")
+const url = "https://api.webflow.com/v2/sites/65a1fff36ec356ca7af685ad/collections"
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    authorization: "Bearer 7b06a025dd94c212ce3b9b0c124a19ed795ead2470da0524229895d0f7537012",
+  },
+}
+
+fetch(url, options)
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.error("error:" + err))
